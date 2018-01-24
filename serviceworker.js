7d21488f4158deba5228487a374374be9ccee49b
Adam Washington <adam.washington@stfc.ac.uk>
2018-01-24 15:52:35 +0000

    Update service worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,1 +1,32 @@
-console.log("Here's a service worker")
+CACHE = "network-or-cache";
+
+self.addEventListener("install", function(evt) {
+    console.log("The service worker is being installed.");
+    evt.waitUntil(precache());
+});
+
+self.addEventListener("fetch", function(evt) {
+    console.log("The service worker is serving the asset.");
+
+    evt.respondWith(fromNetwork(evt.request, 400).catch(function () {
+	return fromCache(evt.request);
+    }));
+});
+
+function precache() {
+    return caches.open(CACHE).then(function (cache) {
+	return cache.addAll(["bus.png", temp.json]);
+    });
+}
+
+function fromNetwork(request, timeout) {
+    return new Promise(function (fulfill, reject) {
+	var timeoutId = setTimeout(reject, timeout);
+
+	fetch(request).then(function (response) {
+	    clearTimeout(timeoutId);
+	    fulfill(response);
+	},
+			    reject);
+    });
+}
