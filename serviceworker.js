11320536f1b768b5fc8701310356676226d1a95b
Adam Washington <adam.washington@stfc.ac.uk>
2018-01-24 16:00:16 +0000

    Remove logging messages from service worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,13 +1,10 @@
 CACHE = "network-or-cache";
 
 self.addEventListener("install", function(evt) {
-    console.log("The service worker is being installed.");
     evt.waitUntil(precache());
 });
 
 self.addEventListener("fetch", function(evt) {
-    console.log("The service worker is serving the asset.");
-
     evt.respondWith(fromNetwork(evt.request, 400).catch(function () {
 	return fromCache(evt.request);
     }));
